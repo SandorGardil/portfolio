73c812eec2f2bfc8ff28b59d46d36f68035ab94b
Sandor Gardil <increpidev@gmail.com>
2024-07-16 22:24:43 -0500

    begone tho-t

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx'
-import './index.css'
 import '../app/globals.css';
 import { ThemeProvider } from './components/theme-provider.tsx'
 
