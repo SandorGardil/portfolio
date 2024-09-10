f73cf5ba1491ce856c4e238cc7cef7bf504ec8e5
Sandor Gardil <sandorg12@gmail.com>
2024-09-09 19:40:06 -0500

    add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "../src/components/ui/tooltip"
+import Navigation from "./components/ui/navigation"
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
                 <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl"><a href="/">Sandor Gardil</a></h1>
                 <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">Full Stack Software Engineer</h2>
                 <p className="mt-4 max-w-xs leading-normal">First name Him, last name Othy</p>
+                <Navigation />
               </div>
               <ul className="ml-1 mt-8 flex items-center">
                 <li className="mr-5 text-xs shrink-0">
@@ -43,19 +45,79 @@ function App() {
             <main id="content" className="pt-24 lg:w-1/2 lg:py-24">
               <section id="about" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="About me">
                   <p className="mb-4">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit adipisci distinctio quae, voluptatibus ab maiores blanditiis eum aspernatur id nostrum, nihil quia? Eius officiis molestiae quisquam maiores libero blanditiis veniam inventore eum voluptatibus delectus. Exercitationem laboriosam dolorum numquam nihil assumenda reprehenderit ipsam ipsum, laudantium incidunt asperiores consequuntur hic vitae cupiditate rerum quia consectetur nemo natus amet. Accusamus laboriosam vel minima magni deserunt excepturi ipsum soluta quaerat culpa? Possimus asperiores nisi excepturi voluptas dignissimos quo doloremque cupiditate ipsum. Libero sequi deserunt quae odit alias dignissimos velit ducimus.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit adipisci distinctio quae, voluptatibus ab maiores blanditiis eum aspernatur id nostrum, nihil quia? Eius officiis molestiae quisquam maiores libero blanditiis veniam inventore eum voluptatibus delectus. Exercitationem laboriosam dolorum numquam nihil assumenda reprehenderit ipsam ipsum, laudantium incidunt asperiores consequuntur hic vitae cupiditate rerum quia consectetur nemo natus amet.
                   </p>
                   <p className="mb-4">
-                    Est quidem, ratione hic quia ea ut autem dolorem eum nesciunt pariatur distinctio natus dolor sequi quos! Nihil quisquam nemo voluptates. Eos soluta pariatur possimus neque dolores. Obcaecati veniam eaque architecto quae veritatis, tempora facere doloremque a id eveniet cumque omnis quod voluptatum! Blanditiis ad, cumque eaque, laborum aperiam vitae eos amet aut vel eligendi dicta error veritatis in? Quis voluptatum necessitatibus animi modi ab eaque sunt. Perferendis, vel libero voluptate facilis, praesentium quia aliquid exercitationem tenetur et enim quis accusantium, voluptates corporis non dolores labore. Rerum ipsum ex hic eum, nulla veritatis facere quam nam optio! Soluta perspiciatis voluptatem rem laboriosam quod? Fugit quisquam praesentium at eveniet libero iusto sint saepe?
+                    Est quidem, ratione hic quia ea ut autem dolorem eum nesciunt pariatur distinctio natus dolor sequi quos! Nihil quisquam nemo voluptates. Eos soluta pariatur possimus neque dolores. Obcaecati veniam eaque architecto quae veritatis, tempora facere doloremque a id eveniet cumque omnis quod voluptatum! Blanditiis ad, cumque eaque, laborum aperiam vitae eos amet aut vel eligendi dicta error veritatis in? Quis voluptatum necessitatibus animi modi ab eaque sunt.
                   </p>
                    
               </section>
-              <section id="expierence" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="Work experience">
+              <section id="experience" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="Work experience">
                 <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
                   <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">Experience</h2>
                 </div>
                 <div>
                   <ol className="group/list">
+                    <li className="mb-12">
+                      <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                        <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                        <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" aria-label="2024 to Present">2024 — Present</header>
+                        <div className="z-10 sm:col-span-6">
+                          <h3 className="font-medium leading-snug text-slate-200">
+                            <div>
+                              <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base" href="https://www.nagios.com/" target="_blank" rel="noreferrer noopener" aria-label="Full Stack Developer, Accessibility at Nagios (opens in a new tab)">
+                                <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                                <span>Full Stack Software Developer · 
+                                  <span className="inline-block ml-1">Nagios
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true">
+                                      <path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd"></path>
+                                    </svg>
+                                  </span>
+                                </span>
+                              </a>
+                            </div>
+                          </h3>
+                          <p className="mt-2 text-sm leading-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam veniam natus voluptas iure nostrum ducimus! Pariatur, sunt eos nulla non deserunt rerum ad ea voluptates porro aut consequuntur dolor, assumenda dolore totam, rem eveniet. Consequuntur quia porro neque nam laboriosam commodi minus inventore non nostrum! Sit accusamus fuga nam. Repellat!</p>
+                          <ul className="mt-2 flex flex-wrap" aria-label="Technologies used">
+                            <li className="mr-1.5 mt-2"><div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">JavaScript</div></li>
+                            <li className="mr-1.5 mt-2"><div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">PHP</div></li>
+                            <li className="mr-1.5 mt-2"><div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">React</div></li>
+                            <li className="mr-1.5 mt-2"><div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">TailWind CSS</div></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li className="mb-12">
+                      <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
+                        <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                        <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" aria-label="2024 to Present">2024 — Present</header>
+                        <div className="z-10 sm:col-span-6">
+                          <h3 className="font-medium leading-snug text-slate-200">
+                            <div>
+                              <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base" href="https://www.nagios.com/" target="_blank" rel="noreferrer noopener" aria-label="Full Stack Developer, Accessibility at Nagios (opens in a new tab)">
+                                <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                                <span>Full Stack Software Developer · 
+                                  <span className="inline-block ml-1">Nagios
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true">
+                                      <path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd"></path>
+                                    </svg>
+                                  </span>
+                                </span>
+                              </a>
+                            </div>
+                          </h3>
+                          <p className="mt-2 text-sm leading-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam veniam natus voluptas iure nostrum ducimus! Pariatur, sunt eos nulla non deserunt rerum ad ea voluptates porro aut consequuntur dolor, assumenda dolore totam, rem eveniet. Consequuntur quia porro neque nam laboriosam commodi minus inventore non nostrum! Sit accusamus fuga nam. Repellat!</p>
+                          <ul className="mt-2 flex flex-wrap" aria-label="Technologies used">
+                            <li className="mr-1.5 mt-2"><div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">JavaScript</div></li>
+                            <li className="mr-1.5 mt-2"><div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">PHP</div></li>
+                            <li className="mr-1.5 mt-2"><div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">React</div></li>
+                            <li className="mr-1.5 mt-2"><div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">TailWind CSS</div></li>
+                          </ul>
+                        </div>
+                      </div>
+                    </li>
+
                     <li className="mb-12">
                       <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
                         <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
