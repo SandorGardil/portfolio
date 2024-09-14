68b62ea9601c28dfa9657fc1e3a21fec0fce771e
Sandor Gardil <sandorg12@gmail.com>
2024-09-14 00:06:14 -0500

    add email icon

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
                 <Navigation />
               </div>
               <ul className="ml-1 mt-8 flex items-center">
-                <li className="mr-5 text-xs shrink-0">
+                <li className="mr-5 text-xs shrink-0 gap-1 flex">
                 <TooltipProvider delayDuration={200}>
                   <Tooltip>
                     <TooltipTrigger>
@@ -37,6 +37,16 @@ function App() {
                     </TooltipTrigger>
                     <TooltipContent>
                       <p>LinkedIn</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+                <TooltipProvider delayDuration={200}>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <a className="block hover:text-slate-200" href="mailto:sandorg12@gmail.com" target="_blank" rel="noreferrer noopener" aria-label="LinkedIn (opens in a new tab)"><span className="sr-only">LinkedIn</span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6" aria-hidden="true"><path d="M19,4H5A3,3,0,0,0,2,7V17a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V7A3,3,0,0,0,19,4Zm-.41,2-5.88,5.88a1,1,0,0,1-1.42,0L5.41,6ZM20,17a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V7.41l5.88,5.88a3,3,0,0,0,4.24,0L20,7.41Z"/></svg></a>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>Email me</p>
                     </TooltipContent>
                   </Tooltip>
                 </TooltipProvider>
