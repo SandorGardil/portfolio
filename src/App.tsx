d3882f9cfe53770579157311f392e4e5206a8a84
Sandor Gardil <sandorg12@gmail.com>
2024-09-15 17:34:05 -0500

    fix interaction where first experience card would break hover styling when hovering the 1 px height sr class

--- src/App.tsx
+++ src/App.tsx
@@ -129,7 +129,7 @@ function App() {
                   </p>
               </section>
               <section id="experience" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="Work experience">
-                <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+                <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:h-[0px] lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
                   <h2 className="text-sm font-bold uppercase tracking-widest text-violet-12 lg:sr-only">Experience</h2>
                 </div>
                 <div>
