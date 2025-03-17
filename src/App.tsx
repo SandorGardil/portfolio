c513272cc8bd866721085d31ddafb439d1ba0723
Sandor <sandorg12@gmail.com>
2025-03-16 22:11:32 -0500

    initiate vercel rebuild

--- src/App.tsx
+++ src/App.tsx
@@ -154,7 +154,7 @@ function App() {
                         technologies={experience.technologies}
                       />
                     ))}
-                  </ol>
+                  </ol> 
                   <div className="mt-12">
                     <a className="inline-flex items-baseline font-medium leading-tight text-violet-12 hover:text-mint focus-visible:text-mint font-semibold text-violet-12 group/link text-base" href="/resume.pdf" target="_blank" rel="noreferrer noopener" aria-label="View Full Resume (opens in a new tab)">
                       <span>View Full <span className="inline-block">Resume<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true"><path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path></svg></span></span>
