98d11869830bc979d4594bd327239f322c08c062
Sandor <sandorg12@gmail.com>
2025-10-22 19:10:37 -0500

    remove project section and updated descriptions and technologies.

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const experienceData = [
     companyUrl: "https://www.nagios.com/",
     jobTitle: "Full Stack Software Developer",
     description:
-      "Led the product management of Nagios Network Analyzer, overseeing its growth and development. Mentored new interns and jr devs, accelerating the on boarding process. Played a key role in the UI overhaul of Nagios Log Server, implementing significant frontend improvements to enhance usability and design.",
+      "Product owner of Nagios Network Analyzer and led the complete rewrite of the project. Mentored new interns and jr devs, accelerating the on boarding process. Played a key role in the UI overhaul of Nagios Log Server, implementing significant frontend improvements to enhance usability and design.",
     technologies: [
       { name: "jQuery" },
       { name: "PHP" },
@@ -29,6 +29,8 @@ const experienceData = [
       { name: "CodeIgniter" },
       { name: "Laravel" },
       { name: "Python" },
+      { name: "RRDtool" },
+      { name: "InfluxDB" },
     ],
   },
   {
@@ -70,14 +72,6 @@ const experienceData = [
     ],
   },
 ];
-
-const sgChatTechnologies = [
-  { name: "Next.js" },
-  { name: "TailwindCSS" },
-  { name: "React" },
-  { name: "InstantDB" },
-]
-
 
 function App() {
   return (
@@ -165,48 +159,6 @@ function App() {
                       <span>View Full <span className="inline-block">Resume<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true"><path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path></svg></span></span>
                     </a>
                   </div>
-                </div>
-              </section>
-              <section id="projects" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24" aria-label="Projects">
-                <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:h-[0px] lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
-                  <h2 className="text-sm font-bold uppercase tracking-widest text-violet-12 lg:sr-only">Projects</h2>
-                </div>
-                <div>
-                  <ol className="group/list">
-                    <li className="mb-12">
-                      <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-                        <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-violet-3/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-                        <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-gray-9 sm:col-span-2" aria-label="sg chat">
-                          {/* image here */}
-                        </header>
-                        <div className="z-10 sm:col-span-6">
-                          <h3 className="font-medium leading-snug text-violet-12">
-                            <div>
-                              <a className="inline-flex items-baseline font-medium leading-tight text-gray-12 hover:text-mint focus-visible:text-mint group/link text-base" href="https://sg-chat-brown.vercel.app/chat" target="_blank" rel="noreferrer noopener" aria-label={`AI chat app used for learning (opens in a new tab)`}>
-                                <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-                                <span>SG Chat
-                                  
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true">
-                                      <path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path>
-                                    </svg>
-                                </span>
-                              </a>
-                            </div>
-                          </h3>
-                          <div className="mt-2 text-sm leading-normal">A work in progress chat app I built for learning new technologies and how to make an app that uses AI. It uses the Gemeni's API to generate responses and the Vercel AI SDK to streamline the process. Motivation was taken from t3.chat for design and snappy UI.</div>
-                          <ul className="mt-2 flex flex-wrap" aria-label="Technologies used">
-                            {sgChatTechnologies.map((tech, index) => (
-                              <li key={index} className="mr-1.5 mt-2">
-                                <div className="flex items-center rounded-full bg-mint/10 px-3 py-1 text-xs font-medium leading-5 text-mint ">
-                                  {tech.name}
-                                </div>
-                              </li>
-                            ))}
-                          </ul>
-                        </div>
-                      </div>
-                    </li>
-                  </ol>
                 </div>
               </section>
             </main>
